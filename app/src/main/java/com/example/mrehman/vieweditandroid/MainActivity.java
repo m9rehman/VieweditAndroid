59c2d784df3371f6aac87c5cc83e630ecf0242c8
m9rehman <m9rehman@uwaterloo.ca>
2017-05-02 13:30:31 -0400

    About to implement Camera preview

--- app/src/main/java/com/example/mrehman/vieweditandroid/MainActivity.java
+++ app/src/main/java/com/example/mrehman/vieweditandroid/MainActivity.java
@@ -4,21 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mlswipeButton;
-    private Button mrswipeButton;
+
+    private LinearLayout mlinlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mlswipeButton = (Button) findViewById(R.id.lswipe);
-        mrswipeButton = (Button) findViewById(R.id.rswipe);
+
+        mlinlayout = (LinearLayout) findViewById(R.id.linlaymain);
+
 
         Intent i = new Intent(this, RegistrationService.class);
         startService(i);
+
+        mlinlayout.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeTop() {
+                Toast.makeText(MainActivity.this, "top", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeLeft() {
+                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeBottom() {
+                Toast.makeText(MainActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 }
