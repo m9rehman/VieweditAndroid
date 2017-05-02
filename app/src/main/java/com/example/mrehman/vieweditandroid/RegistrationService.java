c43df967df9ad0f48e8dada08597a13cceef1941
m9rehman <m9rehman@uwaterloo.ca>
2017-05-02 11:25:48 -0400

    Registration Service done

--- app/src/main/java/com/example/mrehman/vieweditandroid/RegistrationService.java
+++ app/src/main/java/com/example/mrehman/vieweditandroid/RegistrationService.java
@@ -2,6 +2,15 @@ package com.example.mrehman.vieweditandroid;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmPubSub;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import java.io.IOException;
 
 /**
  * Created by mrehman on 2017-05-02.
@@ -14,6 +23,22 @@ public class RegistrationService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent){
+        InstanceID myID = InstanceID.getInstance(this);
+
+        try {
+            String registrationToken = myID.getToken(getString(R.string.gcm_defaultSenderId),
+                    GoogleCloudMessaging.INSTANCE_ID_SCOPE);
+            Log.d("TOKEN",registrationToken);
+
+            GcmPubSub subscription = GcmPubSub.getInstance(this);
+            subscription.subscribe(registrationToken, "/topics/my_little_topic", null);
+
+
+        }
+        catch (IOException ex){
+            Log.e("EXCEPTION", "Device registration failed", ex);
+        }
+
 
     }
 }
