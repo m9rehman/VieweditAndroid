412c7a7cb64734a04a5e71ae239acd4cb9fb6672
m9rehman <m9rehman@uwaterloo.ca>
2017-05-02 11:02:03 -0400

    Created Registration Class

---
+++ app/src/main/java/com/example/mrehman/vieweditandroid/RegistrationService.java
@@ -0,0 +1,19 @@
+package com.example.mrehman.vieweditandroid;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by mrehman on 2017-05-02.
+ */
+
+public class RegistrationService extends IntentService {
+    public RegistrationService() {
+        super("RegistrationService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent){
+
+    }
+}
